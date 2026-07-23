b2ffa26286fb1976358c93bd4a399f6f3bf174df
11callensa <11callensa@gmail.com>
2026-07-23 12:03:44 -0400

    Week 9

---
+++ 07_outputs/Arduino_files/Stepper_Motor/Stepper_Motor.ino
@@ -0,0 +1,25 @@
+#include <AccelStepper.h>
+
+const int stepPin = D0; 
+const int dirPin = D1;   
+
+// Define a stepper and the pins it will use
+AccelStepper stepper(1, stepPin, dirPin); // initialise accelstepper for a two wire board
+
+void setup()
+{
+}
+
+void loop()
+{
+  if (stepper.distanceToGo() == 0)
+  {
+    // Random change to speed, position and acceleration
+    // Make sure we dont get 0 speed or accelerations
+    delay(1000);
+    stepper.moveTo(rand() % 1000);
+    stepper.setMaxSpeed((rand() % 1000) + 1);
+    stepper.setAcceleration((rand() % 1000) + 1);
+  }
+  stepper.run();
+}
